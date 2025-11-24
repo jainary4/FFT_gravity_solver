63739053dcc4e47052fd8514cbba79fc3d94d819
jainary4 <aryanpriyansh544@gmail.com>
2025-11-24 16:27:51 -0500

    added particle system function file

---
+++ Galaxy_Code/particles.h
@@ -0,0 +1,24 @@
+#ifndef PARTICLES_H
+#define PARTICLES_H
+
+#include "structs.h"
+
+// Initialization functions
+void initialize_plummer_positions(Vector3 *positions, int N, float a, Vector3 center);
+void initialize_velocities(Vector3 *velocities, int N, float sigma);
+
+// System operations
+ParticleSystem* create_particle_system(void);
+void destroy_particle_system(ParticleSystem *sys);
+ParticleSystem* initialize_particle_system(void);
+
+// Utilities
+void remove_bulk_motion(ParticleSystem *sys);
+void center_in_box(ParticleSystem *sys, Vector3 box_center);
+
+// Helpers
+// Return pointer to the first black-hole position (array of length N_BH)
+Vector3* get_blackhole_positions(ParticleSystem *sys);
+
+
+#endif
