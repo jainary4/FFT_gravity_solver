7c12f59b93fb0348f3866ee4c819b59884db0ffa
jainary4 <aryanpriyansh544@gmail.com>
2025-11-25 17:24:45 -0500

    Changed float to double

--- Galaxy_Code/mesh.h
+++ Galaxy_Code/mesh.h
@@ -8,10 +8,16 @@ ParticleMesh* create_particle_mesh(void);
 void destroy_particle_mesh(ParticleMesh *pm);
 
 // Grid operations
-float*** allocate_3d_array(int n);
-void free_3d_array(float ***array, int n);
+double*** allocate_3d_array(int n);
+void free_3d_array(double ***array, int n);
 
-// Mass assignment
-void assign_mass_cic(ParticleMesh *pm, ParticleSystem *sys);
+// Mass assignment into a zero-padded grid (no periodic wrapping).
+// rho_pad: padded array of size Np x Np x Np
+// Np: padded grid size (NMESH_PADDED)
+// sys: particle system
+// N: original grid size (NMESH)
+// h: cell size
+// offset: starting index in padded array where original grid block is placed (usually 0 or (Np-N)/2)
+void assign_mass_cic_padded(double ***rho_pad, int Np, ParticleSystem *sys, int N, double h, int offset);
 
 #endif
