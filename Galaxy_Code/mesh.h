d8fef35d483f12814bda30983db0d28936644e79
jainary4 <aryanpriyansh544@gmail.com>
2025-11-26 13:13:20 -0500

    Added acceleration calculation code

--- Galaxy_Code/mesh.h
+++ Galaxy_Code/mesh.h
@@ -20,4 +20,17 @@ void free_3d_array(double ***array, int n);
 // offset: starting index in padded array where original grid block is placed (usually 0 or (Np-N)/2)
 void assign_mass_cic_padded(double ***rho_pad, int Np, ParticleSystem *sys, int N, double h, int offset);
 
+// CIC gather: interpolate grid forces to particle positions and compute accelerations.
+// force_x, force_y, force_z: force grids (padded, size Np x Np x Np)
+// Np: padded grid size
+// sys: particle system (positions and masses)
+// N: original grid size (NMESH)
+// h: cell size
+// offset: starting index in padded array where original grid block is placed
+// Output: particle accelerations stored in sys->accelerations[] (must be pre-allocated)
+void gather_forces_to_particles(
+    double ***force_x, double ***force_y, double ***force_z,
+    int Np, ParticleSystem *sys, int N, double h, int offset
+);
+
 #endif
